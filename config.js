68877c93dfedd64e1303397b2ffc6f10088e1ed5
spoorthyreddych <spoorthych18@gmail.com>
2026-07-10 16:42:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+  const SUPABASE_URL = 'https://ntuzxugxpwcwnzpywweh.supabase.co';
+  const SUPABASE_KEY = 'sb_publishable_XPomKbx5dqy8li8IjN7-_g_sNtpZtJT';
+  const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
